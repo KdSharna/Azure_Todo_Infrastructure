33accb5ea6ff03975724c68ba20b161ea90be5a1
kd.sharma <sharmakapil79027@gmail.com>
2025-12-20 00:43:57 +0530

    Add nginx

--- environment/dev/nginx.sh
+++
@@ -1,9 +0,0 @@
-# sudo apt update
-# sudo apt install nginx -y
-
-# package_update: true
-# packages:
-#   - nginx
-# runcmd:
-#   - systemctl enable nginx
-#   - systemctl start nginx
--- environment/dev/python.sh
+++
@@ -1,8 +0,0 @@
-# sudo apt update -y
-# sudo apt install -y python3 python3-pip
-
-#cloud-config
-# package_update: true
-# packages:
-#   - python3
-#   - python3-pip
