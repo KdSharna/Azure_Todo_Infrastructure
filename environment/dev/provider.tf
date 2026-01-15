d3ff2c0c5736a9ba2fba51e1e3cae77f48c0d0ab
kdsharma <sharmakapil79027@gmail.com>
2026-01-15 23:46:46 +0530

    Refactor provider.tf to use variables for credentials

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -20,4 +20,8 @@ provider "azurerm" {
     }
   }
   subscription_id = "7e450572-1056-4117-9e7b-97e717138408"
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
