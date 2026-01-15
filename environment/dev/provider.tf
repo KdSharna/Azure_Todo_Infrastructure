c8f654f0b9415a1dd43410a25326ad4818b3d303
kdsharma <sharmakapil79027@gmail.com>
2026-01-15 23:47:37 +0530

    Refactor subscription ID to use variable
    Comment out hardcoded subscription ID and use variable.

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = "7e450572-1056-4117-9e7b-97e717138408"
+  #subscription_id = "7e450572-1056-4117-9e7b-97e717138408"
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
